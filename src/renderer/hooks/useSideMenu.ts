1454a518bfe89ea348e5cc189e0468f40c7a7bb8
capybara-sea <g1053356495@gmail.com>
2023-03-16 02:36:25 +0800

    🦄refactor(useSideMenu): 修改了一些路由逻辑

--- src/renderer/hooks/useSideMenu.ts
+++ src/renderer/hooks/useSideMenu.ts
@@ -1,5 +1,5 @@
 import type { MenuOption } from 'naive-ui'
-import { h, ref, computed, watch } from 'vue'
+import { h, ref, computed } from 'vue'
 import { RouterLink, useRouter } from 'vue-router'
 import { menu_home, menu_setting, menu_characters } from '@/assets/icons'
 
@@ -11,7 +11,7 @@ export interface RenderMenuOption {
   icon: string
 }
 
-export const renderMenuOption = (option: RenderMenuOption): MenuOption => {
+const renderMenuOption = (option: RenderMenuOption): MenuOption => {
   return {
     key: option.key,
     label: () => h(option.el, { to: { name: option.to } }, { default: () => option.label }),
@@ -19,36 +19,36 @@ export const renderMenuOption = (option: RenderMenuOption): MenuOption => {
   }
 }
 
+const customMenuOptions = ref<MenuOption[]>([])
+
+const defaultMenuOptions: MenuOption[] = [
+  renderMenuOption({
+    el: RouterLink,
+    to: 'Home',
+    label: '首页',
+    key: 'Home',
+    icon: menu_home,
+  }),
+  renderMenuOption({
+    el: RouterLink,
+    to: 'Characters',
+    label: '角色',
+    key: 'Characters',
+    icon: menu_characters,
+  }),
+  renderMenuOption({
+    el: RouterLink,
+    to: 'Setting',
+    label: '设置',
+    key: 'Setting',
+    icon: menu_setting,
+  }),
+]
+
 export function useSideMenu() {
   // TODO : 和主进程通信，获取侧边栏状态，以及侧边栏自定义菜单内容
   // const appStore = useAppStore()
   // const isSideMenuOpen = computed(() => appStore.isSideMenuOpen)
-
-  const customMenuOptions = ref<MenuOption[]>([])
-
-  const defaultMenuOptions: MenuOption[] = [
-    renderMenuOption({
-      el: RouterLink,
-      to: 'Home',
-      label: '首页',
-      key: 'Home',
-      icon: menu_home,
-    }),
-    renderMenuOption({
-      el: RouterLink,
-      to: 'Characters',
-      label: '角色',
-      key: 'Characters',
-      icon: menu_characters,
-    }),
-    renderMenuOption({
-      el: RouterLink,
-      to: 'Setting',
-      label: '设置',
-      key: 'Setting',
-      icon: menu_setting,
-    }),
-  ]
 
   const menuOptions = computed<MenuOption[]>(() => {
     return [...defaultMenuOptions, ...(customMenuOptions.value as MenuOption[])]
